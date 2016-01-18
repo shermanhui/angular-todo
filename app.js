cfc130de91e76b782259ac7ff0c2be833fe5f1ea
shui91 <sherman.sy.hui@gmail.com>
2016-01-17 16:17:41 -0800

    feat: add routing and angular dependencies

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-var toDoApp = angular.module('toDoApp', []);
+// module
+var toDoApp = angular.module('toDoApp', ['ngRoute', 'ngResource']);
+
+// router
+toDoApp.config(function ($routeProvider){
+	$routeProvider
+
+	.when('/', {
+		templateUrl: 'pages/about.html',
+		controller: 'aboutController'
+	})
+
+	.when('/todo',{
+		templateUrl: 'pages/todo.html',
+		controller: 'todoController'
+	})
+});
+
+// controllers
+toDoApp.controller('aboutController', ['$scope', function($scope){
+
+}]);
+
+toDoApp.controller('todoController', ['$scope', function($scope){
+
+}]);

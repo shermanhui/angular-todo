70a3c0b4bec059a5e296b272bfa5c0644314b762
shui91 <sherman.sy.hui@gmail.com>
2016-01-27 15:54:44 -0800

    feat: add working test specs for todolist

--- test/specs.js
+++ test/specs.js
@@ -1,30 +1,56 @@
 'use strict';
 
-describe('Controller: toDoApp', function () {
+describe('A suite exists', function() {
+
+  it('contains spec with an expectation', function() {
+
+    expect(true).toBe(true);
+
+  });
+});
+
+describe('Controller: todoController', function () {
 
   // load the controller's module
-  beforeEach(module('mytodoApp'));
+  beforeEach(module('toDoApp'));
 
-  var toDoCtrl,
+  var toDoController,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    toDoCtrl = $controller('toDoCtrl', {
+    toDoController = $controller('todoController', {
       $scope: scope
     });
   }));
 
-  it('should add items to the list', function () {
-    scope.addTodo();
-    expect(scope.tasks.length).toBe(1);
+  it('should call add todo', function () {
+  	scope.toDoItem = {title: 'Hello'};
+
+  	spyOn(scope.tasks, "$add").and.returnValue(1);
+  	scope.addToDo();
+  	expect(scope.tasks.$add).toHaveBeenCalled();
+  	// console.log(scope.addToDo);
   });
 
+ //  it('should add an item to the list', function() {
+	// expect(scope.tasks.length).toBe(1);
+ //  });
+
   it('should add then remove an item from the list', function () {
-    scope.addTodo();
-    scope.removeTodo(0);
+  	scope.toDoItem = {title: 'Hello'};
+    scope.addToDo();
+    scope.removeToDo(0);
     expect(scope.tasks.length).toBe(0);
+  });
+
+  it('should mark item as complete', function(){
+
+  });
+
+  it('should mark item as incomplete', function(){
+
   });
 
 });
